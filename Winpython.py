92d9d1222aca693a92769264536763cdb304020e
NymphCoderock <102594756+NymphCoderock@users.noreply.github.com>
2022-03-29 15:50:30 +0100

    Code Is Code!

---
+++ Winpython.py
@@ -0,0 +1,9 @@
+
+import time
+import os
+print ("Winpython 1.0")
+name = input("Enter Username")
+pas = input ("Enter Password")
+time.sleep (10)
+print ("Winpython 1.0")
+print ("Coding Is Cool")
---
+++ setup.py
@@ -0,0 +1,6 @@
+import os
+import time
+print ("Winpython 1.0 Setup")
+time.sleep (15)
+name = input ("Enter Your Username")
+pas = input ("Enter Your Password")
